c3e1216c8102276f4fbc642a222dc5c385e3f766
ShayanJul <s.julaei94@gmail.com>
2022-04-02 14:45:16 +0430

    commit

--- linked_lists/ll_constructor.py
+++ linked_lists/ll_constructor.py
@@ -79,12 +79,86 @@ class LinkedList:
     def pop_first(self):
         """this pops the first item of a linked list
         """
-        if self.length <= 1:
+        if self.length == 0:
             return None
         else:
+            temp = self.head
             self.head = self.head.next
+            temp.next = None
+            self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index):
+        """this gets the item by passing an index
+        """
+        if index < 0 or index >= self.length:
+            return None
+        else:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp
+
+    def set_value(self, index, value):
+        """This sets a value to an index
+        """
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        """This inserts a value at an index
+        """
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length - 1:
+            return self.append(value)
+        else:
+            new_node = Node(value)
+            temp = self.get(index - 1)
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1
+            return True
+
+    def remove(self, index):
+        """This removes an item from an index
+        """
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        """This reverse a linked list
+        """
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
 
 
+# testing the class
 my_linked_list = LinkedList(5)
 print('Original ll is:')
 my_linked_list.print_list()
@@ -103,6 +177,40 @@ my_linked_list.prepend(-2)
 print('New ll after prepend an item is:')
 my_linked_list.print_list()
 
-my_linked_list.pop_first()
-print('New ll after popping the first item is:')
+print('Now let\'s pop first items one by one')
+print(my_linked_list.pop_first())
+print(my_linked_list.pop_first())
+print(my_linked_list.pop_first())
+print(my_linked_list.pop_first())
+print(my_linked_list.pop_first())
+
+print('Adding some new items \nMy new linked list is:')
+my_linked_list.append(-2)
+my_linked_list.append(8)
+my_linked_list.append(4)
+my_linked_list.append(9)
+my_linked_list.append(1)
+my_linked_list.print_list()
+
+print('Get an item by it\'s index:')
+print(my_linked_list.get(2))
+
+print('Set third item with -10:')
+my_linked_list.set_value(2, -10)
+print('New list is:')
+my_linked_list.print_list()
+
+print('Inserting 12 after 2nd item')
+my_linked_list.insert(2, 12)
+print('New list is:')
+my_linked_list.print_list()
+
+print('Let\'s remove the forth item of list')
+my_linked_list.remove(3)
+print('New list is:')
+my_linked_list.print_list()
+
+print('Now let\'s reverse our list')
+my_linked_list.reverse()
+print('Now list is:')
 my_linked_list.print_list()
